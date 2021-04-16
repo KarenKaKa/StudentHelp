58632212da022c780e391a871364cc843ae0fd9e
karen <1107941977@qq.com>
2021-04-16 08:50:43 +0800

    问卷

--- app/src/main/java/com/consultation/studenthelp/net/vo/BaseTableInfo.java
+++ app/src/main/java/com/consultation/studenthelp/net/vo/BaseTableInfo.java
@@ -3,4 +3,8 @@ package com.consultation.studenthelp.net.vo;
 public class BaseTableInfo {
     public static final String OBJECT_ID = "objectId";
     public static final String UPDATED_AT = "updatedAt";
+    public static final String TEACHER_NAME = "teacherName";
+    public static final String TEACHER_ID = "teacherId";
+    public static final String STUDENT_ID = "student_id";
+    public static final String STUDENT_NAME = "student_name";
 }
